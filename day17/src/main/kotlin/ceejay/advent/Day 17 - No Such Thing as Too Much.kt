be68503dd3bb13537ce9739df35ee1f826d99966
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2023-01-09 10:14:30 +0100

    Day 17 completed

---
+++ day17/src/main/kotlin/ceejay/advent/Day 17 - No Such Thing as Too Much.kt
@@ -0,0 +1,56 @@
+package ceejay.advent
+
+import ceejay.advent.util.Day
+import ceejay.advent.util.prependTo
+
+object `Day 17 - No Such Thing as Too Much` : Day<Int, Any>() {
+    override val number = 17
+
+    override fun Sequence<String>.doPart1(): Int {
+        val (liters, containers) = parse()
+        return containers.findAllContainerCombinations(liters).onEach { debug(it) }.count()
+    }
+
+    override fun Sequence<String>.doPart2(): Any {
+        val (liters, containers) = parse()
+        val combinations = containers.findAllContainerCombinations(liters).toList()
+        val minContainersUsed = combinations.minOf { it.size }
+        debug { "Minimum number of containers used to contain $liters liters is $minContainersUsed" }
+        return combinations.count { it.size == minContainersUsed }
+    }
+
+    private fun Sequence<String>.parse(): Pair<Int, List<Int>> {
+        // Added number of liters to first line of input (to differentiate between example and real)
+        val lines = toList()
+        val liters = lines.first().toInt()
+        val containers = lines.drop(1).map { it.toInt() }.sortedDescending()
+        return liters to containers
+    }
+
+    private fun List<Int>.findAllContainerCombinations(remainingLiters: Int): Sequence<List<Int>> {
+        val candidates = filter { it <= remainingLiters }
+        return when (remainingLiters) {
+            0 -> when {
+                candidates.isEmpty() -> sequenceOf(emptyList())
+                else -> emptySequence()
+            }
+
+            else -> sequence {
+                for (i in candidates.indices) {
+                    when (val container = candidates[i]) {
+                        remainingLiters -> yield(listOf(container))
+                        else -> {
+                            val rest: List<Int> =
+                                if (i < candidates.indices.last)
+                                    candidates.subList(i + 1, candidates.size)
+                                else emptyList()
+                            yieldAll(rest.findAllContainerCombinations(remainingLiters - container)
+                                .map { container prependTo it })
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
--- day17/src/test/kotlin/functional/Day17Test.kt
+++ day17/src/test/kotlin/functional/Day17Test.kt
@@ -1,48 +1,46 @@
 package functional
 
-import ceejay.advent.Day17
+import ceejay.advent.`Day 17 - No Such Thing as Too Much`
 import ceejay.advent.util.Input
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-import org.junit.jupiter.api.Disabled
-
-@Disabled
 class Day17Test {
+    private val underTest = `Day 17 - No Such Thing as Too Much`
 
     @Test
     fun `Part 1 returns correct result on example input`() {
         // when
-        val (actual, _) = Day17.part1(Input.Example)
+        val (actual, _) = underTest.part1(Input.Example)
 
         // then
-        assertEquals(TODO(), actual)
+        assertEquals(4, actual)
     }
 
     @Test
     fun `Part 1 returns correct result on real input`() {
         // when
-        val (actual, _) = Day17.part1(Input.Real)
+        val (actual, _) = underTest.part1(Input.Real)
 
         // then
-        assertEquals(TODO(), actual)
+        assertEquals(1304, actual)
     }
 
     @Test
     fun `Part 2 returns correct result on example input`() {
         // when
-        val (actual, _) = Day17.part2(Input.Example)
+        val (actual, _) = underTest.part2(Input.Example)
 
         // then
-        assertEquals(TODO(), actual)
+        assertEquals(3, actual)
     }
 
     @Test
     fun `Part 2 returns correct result on real input`() {
         // when
-        val (actual, _) = Day17.part2(Input.Real)
+        val (actual, _) = underTest.part2(Input.Real)
 
         // then
-        assertEquals(TODO(), actual)
+        assertEquals(18, actual)
     }
 }
